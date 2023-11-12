086754187071f29f97b3101833ad0873283e8a0a
24aeronyx <11211017@student.itk.ac.id>
2023-11-13 00:58:04 +0800

    Update ERD, add drop database query

--- 2-createDB.sql
+++ 2-createDB.sql
@@ -1,5 +1,5 @@
 -- Active: 1699795418222@@127.0.0.1@3306@retail
-create database retail;
+CREATE DATABASE retail;
 
 use retail;
 
---
+++ 9-dropDatabase.sql
@@ -0,0 +1,2 @@
+-- Active: 1699795418222@@127.0.0.1@3306@retail
+DROP DATABASE retail;
