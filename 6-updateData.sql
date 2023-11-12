3012f34260d7faeb998a06693e45b717e72d5312
24aeronyx <11211017@student.itk.ac.id>
2023-11-13 00:17:24 +0800

    Tambahkan query update per tabelnya

--- 6-updateData.sql
+++ 6-updateData.sql
@@ -1,4 +1,43 @@
 -- Active: 1699795418222@@127.0.0.1@3306@retail
+UPDATE categories
+SET
+  name = 'Gaming',
+  description = 'Device atau aksesoris keperluan gaming'
+WHERE id = 2;
+
+UPDATE barang
+SET
+  name = 'Headset Rexus',
+  price = 12000,
+  stock = 30,
+  description = 'Headset Gaming dengan virtual surround 7.1',
+  category_id = 2
+WHERE id = 2;
+
+UPDATE customer
+SET
+  name = 'Ariel',
+  address = 'Jl. Silo',
+  email = 'arielzz@example.com',
+  contact = '085389899996'
+WHERE id = 2;
+
+UPDATE orders
+SET
+  status = 'paid',
+  total = 50000,
+  date = '2023-01-01'
+WHERE id = 2;
+
+UPDATE order_details
+SET
+  order_id = 2,
+  product_id = 5,
+  quantity = 25,
+  price = 20000,
+  subtotal = 200000
+WHERE id = 2;
+
 -- Mengupdate total pada tabel orders berdasarkan total harga barang dari order_details
 UPDATE orders o
 SET total = (
--- 7-deleteData.sql
+++ 7-deleteData.sql
@@ -1,6 +1,11 @@
 -- Delete all
 DELETE FROM categories; ALTER TABLE categories AUTO_INCREMENT = 1;
+DELETE FROM categories WHERE id = 1;
 DELETE FROM barang; ALTER TABLE barang AUTO_INCREMENT = 1;
+DELETE FROM barang WHERE id = 1;
 DELETE FROM customer; ALTER TABLE customer AUTO_INCREMENT = 1;
+DELETE FROM customer WHERE id = 1;
 DELETE FROM orders; ALTER TABLE orders AUTO_INCREMENT = 1;
+DELETE FROM orders WHERE id = 1;
 DELETE FROM order_details; ALTER TABLE order_details AUTO_INCREMENT = 1;
+DELETE FROM order_details WHERE id = 1;
