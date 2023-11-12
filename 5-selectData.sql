d1b20f0c54a4123e3d1617c92d45e68e4323e0e4
24aeronyx <11211017@student.itk.ac.id>
2023-11-13 00:03:27 +0800

    Add select by id

--- 5-selectData.sql
+++ 5-selectData.sql
@@ -1,5 +1,10 @@
 SELECT * FROM categories;
+SELECT * FROM categories WHERE id = 1;
 SELECT * FROM barang;
+SELECT * FROM barang WHERE id = 1;
 SELECT * FROM customer;
+SELECT * FROM customer WHERE id = 1;
 SELECT * FROM orders;
+SELECT * FROM orders WHERE id = 1;
 SELECT * FROM order_details;
+SELECT * FROM order_details WHERE id = 1;
