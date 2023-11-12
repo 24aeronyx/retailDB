1cd643955a34c422c3c36e59ccf03eece9600484
24aeronyx <11211017@student.itk.ac.id>
2023-11-12 23:44:19 +0800

    Case 1 done

--- case1.sql
+++ case1.sql
@@ -7,6 +7,19 @@ SET @order_id = LAST_INSERT_ID();
 
 -- Menyisipkan detail pesanan (3 barang yang berbeda) ke dalam tabel order_details
 INSERT INTO order_details (order_id, product_id, quantity, price, subtotal) VALUES
-(@order_id, 1, 2, 10, 20), -- Barang pertama (ID produk 1, jumlah 2, harga 10 per barang)
-(@order_id, 2, 1, 15, 15), -- Barang kedua (ID produk 2, jumlah 1, harga 15 per barang)
-(@order_id, 3, 3, 8, 24); -- Barang ketiga (ID produk 3, jumlah 3, harga 8 per barang)
+(@order_id, 1, 2, 5000000, 10000000), -- Laptop ASUS
+(@order_id, 2, 1, 150000, 150000), -- Celana Jeans Pria
+(@order_id, 3, 3, 3000, 9000); -- Pensil HB
+
+-- Cek apakah 3 barang telah masuk dengan order_id yang sama?
+SELECT
+    od.id AS order_detail_id,
+    od.order_id,
+    b.name AS product_name,
+    od.quantity,
+    od.price,
+    od.subtotal
+FROM
+    order_details od
+JOIN
+    barang b ON od.product_id = b.id;
